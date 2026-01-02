ddf0fcdc29078f20c74b39d29d67546ffe7cebf1
Isaac Franks <isaac@yulife.com>
2026-01-02 12:49:55 +0000

    Initial Commit

---
+++ Vocap/VocapApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocapApp.swift
+//  Vocap
+//
+//  Created by Isaac Franks on 02/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct VocapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VocapTests/VocapTests.swift
@@ -0,0 +1,17 @@
+//
+//  VocapTests.swift
+//  VocapTests
+//
+//  Created by Isaac Franks on 02/01/2026.
+//
+
+import Testing
+@testable import Vocap
+
+struct VocapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
